9d52c240b95ce54587146419fd735eb9f69dbb1a
abhisjsu <abhishek.chaudhary@sjsu.edu>
2017-03-17 21:53:01 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+'''
+Filename : app.py
+Latest update : 17th March 2017
+Description : This is the main application file which implement the routing logic presented in the browser
+			  This application make use of Flask Api 
+
+Created by : Abhishek Chaudhary
+'''
+
+############################################## IMPORTING THE REQUIRED MODULES ##################################################
+import sys
+from github import Github
+from flask import Flask
+
+############################################## IMPORTING THE REQUIRED MODULES ##################################################
+
+app = Flask(__name__)
+
+# getting the github credentials authenticated by passing them as an argument to the Github constructor
+g = Github('abhisjsu','Abhi@1234');
+user = g.get_user()
+
+#Getting the URL to the github repo which is explicitly passed as a command line argument by the user
+gitUrl = sys.argv[1];
+
+#Splitting the github URL and obtaining the name of the repository whose files need to be accessed
+gitUrl = gitUrl.split("/");
+repo = user.get_repo(gitUrl[-1])
+
+
+
+############################################## ESTABLISHING THE NAVIGATION ROUTES ##################################################
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/<filename>-config.json")
+def msgJson(filename):
+	file_path = filename+"-config.json"
+	file = repo.get_file_contents(file_path)
+	return "%s"%file.decoded_content
+
+@app.route("/<filename>-config.yml")
+def profile(filename):
+	file_path = filename+"-config.yml"
+	file = repo.get_file_contents(file_path)
+
+	return "%s"%file.decoded_content
+
+if __name__ == "__main__":
+    app.run()
+
+##################################################### END OF FILE #########################################################
